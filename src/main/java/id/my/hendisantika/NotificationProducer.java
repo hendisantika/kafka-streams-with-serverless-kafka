d77ed9d48eb77d818a571299c4c94a523fdc53d4
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 07:14:10 +0700

    Add NotificationProducer 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/NotificationProducer.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-streams-with-serverless-kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 07:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class NotificationProducer {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+}
