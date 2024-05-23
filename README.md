7cd9c07f0f649bb5c699e7a66ef2aad717429542
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 07:06:15 +0700

    Create Gradle Project 🫸🌀✏️📗🐧🐳

---
+++ README.md
@@ -0,0 +1,1 @@
+# kafka-streams-with-serverless-kafka
---
+++ src/main/java/id/my/hendisantika/Main.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Default (Template) Project
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 07:03
+ * To change this template use File | Settings | File Templates.
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
