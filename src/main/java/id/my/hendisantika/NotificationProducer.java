ff627f0f11fcf5d108ee78e87e8cb1457373036d
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 07:14:31 +0700

    Add sendNotification 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/NotificationProducer.java
+++ src/main/java/id/my/hendisantika/NotificationProducer.java
@@ -1,6 +1,11 @@
 package id.my.hendisantika;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +19,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class NotificationProducer {
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public void sendNotification(NotificationDTO notificationDTO, Properties propertiesFromFile) {
+        try {
+            Properties kafkaProducerProperties = getKafkaProducerProperties(propertiesFromFile);
+
+            String topicName = propertiesFromFile.get("kafka.notification.topic").toString();
+
+            try (var producer = new KafkaProducer<String, String>(kafkaProducerProperties)) {
+
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(
+                        topicName,
+                        notificationDTO.getNotificationId().toString(),
+                        objectMapper.writeValueAsString(notificationDTO));
+
+                producer.send(producerRecord);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
