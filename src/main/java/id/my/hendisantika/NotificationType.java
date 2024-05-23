1fc8ca483ec7c9ebd29544df2d2f3dac65136561
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 07:13:30 +0700

    Add NotificationType 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/NotificationType.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-streams-with-serverless-kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 07:13
+ * To change this template use File | Settings | File Templates.
+ */
+public enum NotificationType {
+    PUSH_NOTIFICATION, SMS, EMAIL
+}
